6d7534a623b62af5c92f29fc57b6d1d51f7e973a
Owen Leonard <oleonard15@gmail.com>
2018-07-15 14:58:58 -0400

    changing response

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(cors());
 
 
-app.get('/', (req, res) => {res.send(database.users);})
+app.get('/', (req, res) => {res.send('it is working!');})
 
 app.post('/signin', (req,res) => {signin.handleSignIn(req, res, db, bcrypt)})
 
@@ -39,6 +39,6 @@ app.put('/image', (req,res) => {image.handleImagePut(req,res,db)})
 app.post('/imageurl', (req, res) => {image.handleApiCall(req,res)})
 
 app.listen(process.env.PORT || 3000, () => { 
-    console.log(`app is running on port ${process.env.port}`)
+    console.log(`app is running on port ${process.env.PORT}`)
 });
 
