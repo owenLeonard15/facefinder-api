66d364c0305d21a8f05a40c5dfb5b25436c51f3a
Owen Leonard <oleonard15@gmail.com>
2018-07-15 15:59:03 -0400

    adding database connection

--- server.js
+++ server.js
@@ -12,10 +12,8 @@ const image = require('./controllers/image');
 const db = knex({
     client: 'pg',
     connection: {
-      host : 'postgresql-flexible-44928',
-      user : 'postgres',
-      password : 'Otrain15$',
-      database : 'facefinder'
+      host : process.env.DATABASE_URL,
+      ssl: true
     }
   });
 
